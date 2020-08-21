66d386adefaaa87d6b2757d4cefda35db61df87b
thomas6724 <54256541+thomas6724@users.noreply.github.com>
2020-08-21 19:34:37 +1200

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,4 @@
+from spellchecker import SpellChecker
+
+SPELL = SpellChecker()
+LINK = "https://www.newegg.com/p/pl?d="
---
+++ run_webscraper.py
@@ -0,0 +1,4 @@
+import functions
+
+# Importing a function called run() from a secondary document called functions
+functions.run()
